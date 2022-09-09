b3a0de013f9ce4b76b4864d8c2f7bffc52afba2a
Benj <Benjamin.Schwerdtner@gmail.com>
2022-09-09 14:31:19 +0200

    Update stuff

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Just a toy website.
+This is up on https://SingularityGroup.github.io/yerba-mate-is-love
+This is one of the variations of me tyring to make the simplest
+website possible.
+
+I made a video of me making this website, including creating the repo
+and fumbling around.
+
+https://drive.google.com/file/d/1HCIoAEXiq5-hLoO4ap0hXSdcv8oqVzM1/view?usp=sharing
--- src/pages/index.clj
+++ src/pages/index.clj
@@ -8,8 +8,13 @@
  (format
   (slurp (io/resource "base.html"))
   (h/html
-   [:h1 "yerba mate is love"
+   [:div
+    [:h1 "yerba mate is love"]
+    [:h2 "Prepare"]
+    [:div "Put some herb into the gourd."]
+    [:div "Shake and move the mate to one side."]
+    [:div "Awaken the mate with cold water."]
+    [:div "Be patient and do not stir the mate."]
     [:div
-     [:h2 "Prepare"]
-     [:div "Do not stir the mate"]
-     [:div "Really do not stir it"]]])))
+     {:style {"font-weight" "bold" "color" "OrangeRed"}}
+     "Really, do not stir it"]])))
