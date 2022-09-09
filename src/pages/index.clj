ad30d8561e998b5f222602162852f8c6f13b9000
Benj <Benjamin.Schwerdtner@gmail.com>
2022-09-09 14:03:02 +0200

    Add stuff

--- src/pages/index.clj
+++ src/pages/index.clj
@@ -3,10 +3,13 @@
    [clojure.java.io :as io]
    [hiccup2.core :as h]))
 
-
-
 (spit
  "public/index.html"
  (format
   (slurp (io/resource "base.html"))
-  (h/html [:h1 "hello"])))
+  (h/html
+   [:h1 "yerba mate is love"
+    [:div
+     [:h2 "Prepare"]
+     [:div "Do not stir the mate"]
+     [:div "Really do not stir it"]]])))
--- src/pages/main.clj
+++
@@ -1,5 +0,0 @@
-(ns pages.main
-  (:require [hiccup2.core :as h]))
-
-
-(h/html [:h1 "hello"])
